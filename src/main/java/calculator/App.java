35a4f3082605439f61d1f176331e51ed92fdcddb
Robbie <robbie331@>
2025-07-12 14:39:47 +0900

    level1_2 사칙 연산기호 입력받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,11 +10,14 @@ public class App {
         Scanner scanner = new Scanner(System.in);
 
 
-        //인수는 양의 정수
+
 
         try {
-            int firstNum = 0;
-            int secondNum = 0;
+            int firstNum;
+            int secondNum;
+            int operator;
+
+            //인수는 양의 정수
             while (true) {
                 System.out.print("첫 번째 인수를 입력하세요: ");
                  firstNum = scanner.nextInt();
@@ -25,21 +28,32 @@ public class App {
                     break;
                 }
             }
+            //인수는 양의 정수
+            while (true) {
+                System.out.print("두 번째 인수를 입력하세요: ");
+                secondNum = scanner.nextInt();
 
-                while (true) {
-                    System.out.print("두 번째 인수를 입력하세요: ");
-                    secondNum = scanner.nextInt();
-
-                    if (secondNum < 0) {
-                        System.out.println("인수는 양수의 값(0 포함)을 가집니다.");
-                    } else {
-                        break;
-                    }
+                if (secondNum < 0) {
+                    System.out.println("인수는 양수의 값(0 포함)을 가집니다.");
+                } else {
+                    break;
                 }
+            }
+
+            //사칙연산 기호는 +, -, *, /
+            while (true) {
+                System.out.print("사칙 연산기호를 입력하세요: ");
+                operator = scanner.next().charAt(0);
+
+                if(operator == '+' || operator == '-' || operator == '*' || operator =='/'){
+                    break;
+                }
+            }
 
 
-                System.out.println("firstNum = " + firstNum);
-                System.out.println("secondNum = " + secondNum);
+            System.out.println("firstNum = " + firstNum);
+            System.out.println("secondNum = " + secondNum);
+            System.out.println("operator = " + operator);
         }catch(InputMismatchException e){
 
             System.out.println("InputMismatchException");
