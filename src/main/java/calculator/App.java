3cf6dbc1d889a114e4044cdc361015e6fcc96419
Robbie <robbie331@>
2025-07-12 17:12:33 +0900

    level1 inquiry 출력 후 \n문자 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -113,6 +113,7 @@ public class App {
                     for(String result : results){
                         System.out.print(result + ", ");
                     }
+                    System.out.println("\n");
                 }
             }
 
